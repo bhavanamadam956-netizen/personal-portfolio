bedbd724b435de97d346fa76fd4816ba0ce73925
bhavanamadam956-netizen <bhavanamadam956@gmail.com>
2026-09-16 20:10:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const themeToggle = document.getElementById("themeToggle");
 
+themeToggle.addEventListener("click", () => {
+
+    const currentTheme =
+        document.documentElement.getAttribute("data-theme");
+
+    if (currentTheme === "dark") {
+
+        document.documentElement.removeAttribute("data-theme");
+
+        themeToggle.textContent = "🌙 Dark Mode";
+
+    } else {
+
+        document.documentElement.setAttribute("data-theme", "dark");
+
+        themeToggle.textContent = "☀️ Light Mode";
+    }
+});
